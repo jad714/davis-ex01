2d09cf243f26bd6c682882f9f34b6618be849cf4
jad714 <josh01100100@gmail.com>
2021-09-02 13:00:35 -0400

    Finished Exercise 1

--- src/main/java/Solution01.java
+++ src/main/java/Solution01.java
@@ -1,4 +1,22 @@
-public class Solution01 {
+import java.util.Scanner;
 
+public class Solution01 {
+/* Begin pseudocode */
+    // Create variable to store username (can contain greeting) (type: String).
+    // Create Scanner variable.
+    // Create variable to store string read by the variable.
+    // Prompt the user for their name.
+    // Use concat() to join the two strings (set first variable to concatenated value.
+    // Print variable.
+/* End pseudocode */
+
+    public static void main(String[] args){
+        String greet = "Hello, ";
+        Scanner nameRead = new Scanner(System.in);
+        System.out.println("What is your name? ");
+        String name = nameRead.nextLine();
+        greet = greet.concat(name);
+        System.out.println(greet + ", nice to meet you!");
+    }
 
 }
